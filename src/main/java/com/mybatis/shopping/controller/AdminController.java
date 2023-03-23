6b6eed7572e36336b393dfd97fb25f30c7e1a852
SUJINJEONG012 <peekaboo32@naver.com>
2023-03-23 22:26:35 +0900

    관리자 컨트롤러 작가,상품등록관리페이지 맵핑

--- src/main/java/com/mybatis/shopping/controller/AdminController.java
+++ src/main/java/com/mybatis/shopping/controller/AdminController.java
@@ -11,12 +11,35 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class AdminController {
 
 	private static final Logger logger = LoggerFactory.getLogger(AdminController.class);
-	
+
 	/* 관리자 메인페이지로 이동 */
 	@GetMapping("/main")
 	public void adminMainGet() throws Exception {
 		logger.info("관리자 페이지로 이동");
 	}
-	
-	
+
+	/* 상품 등록 페이지 */
+	@GetMapping("/goodsMange")
+	public void goodsMangeGet() throws Exception {
+		logger.info("상품등록 페이지 접속");
+	}
+
+	/* 상품 등록 페이지 */
+	public void goodsEnrollGet() throws Exception {
+		logger.info("상품등록 페이지 접속");
+	}
+
+	/* 작가 등록 페이지 */
+	@GetMapping("/authorEnroll")
+	public void authorEnrollGet() throws Exception {
+		logger.info("작가등록 페이지접속");
+	}
+
+	/* 작가 관리 페이지 */
+	@GetMapping("/authorManage")
+	public void authorMangeGet() throws Exception {
+		
+		logger.info("작가관리페이지 접속");
+	}
+
 }
