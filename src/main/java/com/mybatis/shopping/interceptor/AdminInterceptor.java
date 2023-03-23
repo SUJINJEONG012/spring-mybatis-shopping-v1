2a4fbceffff01dc0668f06aa854ca8c19b23371e
angela <peekaboo32@naver.com>
2023-03-23 16:53:54 +0900

    admin/main 으로 url 들어가도 메인으로 이동하게끔

--- src/main/java/com/mybatis/shopping/interceptor/AdminInterceptor.java
+++ src/main/java/com/mybatis/shopping/interceptor/AdminInterceptor.java
@@ -1,5 +1,26 @@
 package com.mybatis.shopping.interceptor;
 
-public class AdminInterceptor {
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
+import org.springframework.web.servlet.HandlerInterceptor;
+
+import com.mybatis.shopping.model.MemberVo;
+
+public class AdminInterceptor implements HandlerInterceptor {
+
+	@Override
+	public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+		
+		HttpSession session = request.getSession();
+		MemberVo lvo = (MemberVo) session.getAttribute("member");
+		if(lvo == null || lvo.getAdminCk() == 0) {
+			response.sendRedirect("/");
+			return false;
+		}
+		
+		return true; //관리자 계정인 경우 true
+	}
+	
 }
--- src/main/java/com/mybatis/shopping/interceptor/LoginInterceptor.java
+++ src/main/java/com/mybatis/shopping/interceptor/LoginInterceptor.java
@@ -1,5 +1,18 @@
 package com.mybatis.shopping.interceptor;
 
-public class LoginInterceptor {
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
+import org.springframework.web.servlet.HandlerInterceptor;
+
+public class LoginInterceptor implements HandlerInterceptor {
+
+	@Override
+	public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+		System.out.println("LoginInterceptor preHandle 작동");
+		HttpSession session = request.getSession();
+		session.invalidate();
+		return true;
+	}
 }
