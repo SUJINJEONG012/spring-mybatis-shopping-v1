e943f36ee450769c37201fbda08ad531aaeab010
SUJINJEONG012 <peekaboo32@naver.com>
2023-03-27 23:03:37 +0900

    서비스에 작가목록 메서드 추가하기

--- src/main/java/com/mybatis/shopping/service/AuthorService.java
+++ src/main/java/com/mybatis/shopping/service/AuthorService.java
@@ -1,8 +1,15 @@
 package com.mybatis.shopping.service;
 
+import java.util.List;
+
 import com.mybatis.shopping.model.AuthorVo;
+import com.mybatis.shopping.model.Criteria;
 
 public interface AuthorService {
-	
+	/* 작가등록 */
 	public void authorEnrol(AuthorVo authorVo) throws Exception;
+	
+	/* 작가 목록 */
+	public List<AuthorVo> authorGetList(Criteria cri) throws Exception;
+
 }
