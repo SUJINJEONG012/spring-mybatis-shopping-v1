18bb59b7ab1c22bb9fae16405e14c50cdea9dd62
angela <peekaboo32@naver.com>
2023-03-31 17:05:07 +0900

    getter, setter, toString 메서드 생성

--- src/main/java/com/mybatis/shopping/model/BookVo.java
+++ src/main/java/com/mybatis/shopping/model/BookVo.java
@@ -20,6 +20,107 @@ public class BookVo {
 	private String bookContents; // 상품 목차
 	private Date regDate; // 등록날짜
 	private Date updateDate; //수정날짜
+	public int getBookId() {
+		return bookId;
+	}
+	public void setBookId(int bookId) {
+		this.bookId = bookId;
+	}
+	public String getBookName() {
+		return bookName;
+	}
+	public void setBookName(String bookName) {
+		this.bookName = bookName;
+	}
+	public int getAuthorId() {
+		return authorId;
+	}
+	public void setAuthorId(int authorId) {
+		this.authorId = authorId;
+	}
+	public int getAuthName() {
+		return authName;
+	}
+	public void setAuthName(int authName) {
+		this.authName = authName;
+	}
+	public String getPubleYerar() {
+		return publeYerar;
+	}
+	public void setPubleYerar(String publeYerar) {
+		this.publeYerar = publeYerar;
+	}
+	public String getPublisher() {
+		return publisher;
+	}
+	public void setPublisher(String publisher) {
+		this.publisher = publisher;
+	}
+	public String getCateCode() {
+		return cateCode;
+	}
+	public void setCateCode(String cateCode) {
+		this.cateCode = cateCode;
+	}
+	public String getCateName() {
+		return cateName;
+	}
+	public void setCateName(String cateName) {
+		this.cateName = cateName;
+	}
+	public int getBookPrice() {
+		return bookPrice;
+	}
+	public void setBookPrice(int bookPrice) {
+		this.bookPrice = bookPrice;
+	}
+	public int getBookStock() {
+		return bookStock;
+	}
+	public void setBookStock(int bookStock) {
+		this.bookStock = bookStock;
+	}
+	public double getBookDiscount() {
+		return bookDiscount;
+	}
+	public void setBookDiscount(double bookDiscount) {
+		this.bookDiscount = bookDiscount;
+	}
+	public String getBookIntro() {
+		return bookIntro;
+	}
+	public void setBookIntro(String bookIntro) {
+		this.bookIntro = bookIntro;
+	}
+	public String getBookContents() {
+		return bookContents;
+	}
+	public void setBookContents(String bookContents) {
+		this.bookContents = bookContents;
+	}
+	public Date getRegDate() {
+		return regDate;
+	}
+	public void setRegDate(Date regDate) {
+		this.regDate = regDate;
+	}
+	public Date getUpdateDate() {
+		return updateDate;
+	}
+	public void setUpdateDate(Date updateDate) {
+		this.updateDate = updateDate;
+	}
+	@Override
+	public String toString() {
+		return "BookVo [bookId=" + bookId + ", bookName=" + bookName + ", authorId=" + authorId + ", authName="
+				+ authName + ", publeYerar=" + publeYerar + ", publisher=" + publisher + ", cateCode=" + cateCode
+				+ ", cateName=" + cateName + ", bookPrice=" + bookPrice + ", bookStock=" + bookStock + ", bookDiscount="
+				+ bookDiscount + ", bookIntro=" + bookIntro + ", bookContents=" + bookContents + ", regDate=" + regDate
+				+ ", updateDate=" + updateDate + "]";
+	}
+	
+	
+	
 	
 	
 }
