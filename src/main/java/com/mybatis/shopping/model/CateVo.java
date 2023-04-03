654e8143e5e4b27ee8b4bd015b33120ded6d61b3
angela <peekaboo32@naver.com>
2023-04-03 10:48:41 +0900

    카테고리 VO생성

---
+++ src/main/java/com/mybatis/shopping/model/CateVo.java
@@ -0,0 +1,57 @@
+package com.mybatis.shopping.model;
+
+public class CateVo {
+	
+	/* 카테고리 등급 */
+	private int tier;
+	
+	/* 카테고리 이름 */
+	private String cateName;
+	
+	/* 카테고리 넘버 */
+	private String cateCode;
+	
+	/* 상위 카테고리 */
+	private String cateParent;
+
+	public int getTier() {
+		return tier;
+	}
+
+	public void setTier(int tier) {
+		this.tier = tier;
+	}
+
+	public String getCateName() {
+		return cateName;
+	}
+
+	public void setCateName(String cateName) {
+		this.cateName = cateName;
+	}
+
+	public String getCateCode() {
+		return cateCode;
+	}
+
+	public void setCateCode(String cateCode) {
+		this.cateCode = cateCode;
+	}
+
+	public String getCateParent() {
+		return cateParent;
+	}
+
+	public void setCateParent(String cateParent) {
+		this.cateParent = cateParent;
+	}
+
+	@Override
+	public String toString() {
+		return "CateVo [tier=" + tier + ", cateName=" + cateName + ", cateCode=" + cateCode + ", cateParent="
+				+ cateParent + "]";
+	}
+	
+	
+	
+}
