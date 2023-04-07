12b2c965639266aba6b03a9bee806e92702e1860
angela <peekaboo32@naver.com>
2023-04-07 09:54:20 +0900

    mapper 테스트  상품조회 데이터 콘솔창으로 조회

--- src/test/java/com/mybatis/shopping/mapper/AdminMapperTests.java
+++ src/test/java/com/mybatis/shopping/mapper/AdminMapperTests.java
@@ -1,14 +1,12 @@
 package com.mybatis.shopping.mapper;
 
-import java.util.List;
-
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
-import com.mybatis.shopping.model.Criteria;
+import com.mybatis.shopping.model.BookVo;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration("file:src/main/webapp/WEB-INF/spring/root-context.xml")
@@ -44,24 +42,33 @@ public class AdminMapperTests {
 //	}
 	
 	/* 상품리스트 및 상품 총 개수 테스트 */
+//	@Test
+//	public void goodsGetListTest() {
+//		
+//		Criteria cri = new Criteria();
+//		
+//		//검색조건
+//		cri.setKeyword("테스트");
+//		
+//		
+//		//검색 리스트
+//		List list = adminMapper.goodsGetList(cri);
+//		for(int i = 0; i< list.size(); i++) {
+//			System.out.println("result........." +  i + " : "  + list.get(i));
+//		}
+//		
+//		int result = adminMapper.goodsGetTotal(cri);
+//		System.out.println("result........" + result);
+//		
+//	}
+	
+	
+	/* 상품 조회 페이지 */
 	@Test
-	public void goodsGetListTest() {
-		
-		Criteria cri = new Criteria();
-		
-		//검색조건
-		cri.setKeyword("테스트");
-		
-		
-		//검색 리스트
-		List list = adminMapper.goodsGetList(cri);
-		for(int i = 0; i< list.size(); i++) {
-			System.out.println("result........." +  i + " : "  + list.get(i));
-		}
-		
-		int result = adminMapper.goodsGetTotal(cri);
-		System.out.println("result........" + result);
-		
+	public void goodsGetDetailTest() {
+		int bookId = 150;
+		BookVo result = adminMapper.goodsGetDetail(bookId);
+		System.out.println("상품조회 데이터 : " + result);
 	}
 	
 }
