777b9f61c53dad78be56282098a664a326baa7bf
angela <peekaboo32@naver.com>
2023-05-04 13:50:03 +0900

    파일경로 윈도우로  변경하고 task 테스트 코드 작성

--- src/test/java/com/mybatis/shopping/task/TaskTest.java
+++ src/test/java/com/mybatis/shopping/task/TaskTest.java
@@ -1,8 +1,11 @@
 package com.mybatis.shopping.task;
 
+import java.io.File;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.List;
 
 import org.junit.Test;
@@ -22,6 +25,15 @@ public class TaskTest {
 	@Autowired
 	private AdminMapper adminMapper;
 	
+	
+	private String getFolderYesterDay() {
+		SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+		Calendar cal = Calendar.getInstance();
+		cal.add(Calendar.DATE, -1);
+		String str = sdf.format(cal.getTime());
+		return str.replace("-", File.separator);
+	}
+	
 	@Test
 	public void taskTests() {
 
@@ -32,17 +44,24 @@ public class TaskTest {
 		
 		fileList.forEach(list -> System.out.println(list));
 		
+		// 비교 기준 파일 리스트 path객체
 		List<Path> checkFilePath = new ArrayList<Path>();
 		
+		//원본이미지
 		fileList.forEach(bookVo ->{
-			Path path = Paths.get("/Users/jeongsujin/upload", bookVo.getUploadPath(), bookVo.getUuid() + "_" + bookVo.getFileName());
-			//Path path = Paths.get("C:\\upload", bookVo.getUploadPath(), bookVo.getUuid() + "_" + bookVo.getFileName());
-			
+			//Path path = Paths.get("/Users/jeongsujin/upload", bookVo.getUploadPath(), bookVo.getUuid() + "_" + bookVo.getFileName());
+			Path path = Paths.get("C:\\upload", bookVo.getUploadPath(), bookVo.getUuid() + "_" + bookVo.getFileName());
 			checkFilePath.add(path);
 		});
 		
-		System.out.println("checkFilePath : ");
+		//썸네일 이미지
+		fileList.forEach(bookVo -> {
+			Path path = Paths.get("C:\\upload", bookVo.getUploadPath(), "s_" + bookVo.getUuid() + "_" + bookVo.getFileName());
+			checkFilePath.add(path);
+		});
 		
+		
+		System.out.println("checkFilePath : ");
 		checkFilePath.forEach(list -> System.out.println(list));
 		System.out.println("==========================");	
 		
