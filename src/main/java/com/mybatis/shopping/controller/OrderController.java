3ce166adc8410f02482727e09967a25883426578
angela <peekaboo32@naver.com>
2023-05-16 09:29:32 +0900

    백업하고 다시 수정하기

--- src/main/java/com/mybatis/shopping/controller/OrderController.java
+++ src/main/java/com/mybatis/shopping/controller/OrderController.java
@@ -19,35 +19,21 @@ import com.mybatis.shopping.model.OrderDto;
 import com.mybatis.shopping.model.OrderPageDto;
 import com.mybatis.shopping.service.MemberService;
 import com.mybatis.shopping.service.OrderService;
+import com.mybatis.shopping.model.OrderPageDto;
+import com.mybatis.shopping.service.MemberService;
 
 @Controller
 public class OrderController {
 	
 	private static final Logger logger = LoggerFactory.getLogger(AdminController.class);
-
-	
-	
 	@Autowired
 	private  OrderService orderService;
-=======
 
-import com.mybatis.shopping.model.OrderPageDto;
-import com.mybatis.shopping.service.MemberService;
-//import com.mybatis.shopping.service.OrderService;
-
-@Controller
-public class OrderController {
-
-	//@Autowired
-	//private  OrderService orderService;
->>>>>>> 7bffc37... 뷰페이지에 장바구니에 담은dto 리스트로 콘솔창에 나오는지 체크
-	
 	@Autowired
 	private  MemberService memberService;
 	
 	
 	@GetMapping("/order/{memberId}")
-<<<<<<< HEAD
 	public String orderPageGet(@PathVariable("memberId") String memberId, OrderPageDto opd, Model model) {
 		
 		//System.out.println("memberId : "+ memberId);
@@ -66,16 +52,6 @@ public class OrderController {
 		System.out.println(od);		
 		logger.info("orderController 적용 여부 ");
 		return "redirect:/";
-=======
-	public void orderPageGet(@PathVariable("memberId") String memberId, OrderPageDto opd, Model model) {
-		
-		System.out.println("memberId : "+ memberId);
-		System.out.println("orders : " + opd.getOrders());
-		
-		//model.addAttribute("orderList", orderService.getGoodsInfo(opd.getOrders()));
-		//model.addAttribute("memberInfo",memberService.getMemberInfo(memberId));
-		
-		//return "/order";
->>>>>>> 7bffc37... 뷰페이지에 장바구니에 담은dto 리스트로 콘솔창에 나오는지 체크
-	}
+	
+    }
 }
